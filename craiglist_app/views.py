a664acbb0fdb09b6fea25c6f00da0720729b6571
jitbhuin07 <jitbhuin07@gmail.com>
2021-04-01 11:32:34 +0530

    added price filter

--- craiglist_app/views.py
+++ craiglist_app/views.py
@@ -18,8 +18,18 @@ def new_search(request):
     # return HttpResponse({'new search is working'})
     # print(request.POST)
     search = request.POST.get('search')
+    if request.POST.get('min_price'):
+        min_price = int(request.POST.get('min_price'))
+    else:
+        min_price=0
+    if request.POST.get('max_price'):
+        max_price = int(request.POST.get('max_price'))
+    else:
+        max_price=10000000000
     location =request.POST.get('location')
     print(location)
+    print((min_price))
+    print(max_price)
     final_url = BASE_CRAIGLIST_URL.format(location,quote_plus(search))
     print(final_url)
     models.SearchModel.objects.create(search=search, create_time=timezone.now())
@@ -29,19 +39,27 @@ def new_search(request):
     post_listings = soup.find_all('li', {'class': 'result-row'})
     final_postings = []
     for post in post_listings:
-        post_title = post.find(class_='result-title').text
-        post_url = post.find('a').get('href')
         if post.find(class_='result-price'):
-            post_price = post.find(class_='result-price').text
-        else:
-            post_price = 'N/A'
-        if post.find(class_='result-image').get('data-ids'):
-            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
-            post_image = BASE_IMAGE_URL.format(post_image_id)
-        else:
-            post_image = 'https://craigslist.org/images/peace.jpg'
+            price_csv = post.find(class_='result-price').text[1:].split(',')
+            post_price=""
+            for price in price_csv:
+                post_price+=price
+            if int(post_price)>=min_price and int(post_price)<=max_price:
 
-        final_postings.append((post_title, post_url, post_price,post_image))
+                post_title = post.find(class_='result-title').text
+                post_url = post.find('a').get('href')
+                post_price=post.find(class_='result-price').text
+                # if post.find(class_='result-price'):
+
+                # else:
+                #     post_price = 'N/A'
+                if post.find(class_='result-image').get('data-ids'):
+                    post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+                    post_image = BASE_IMAGE_URL.format(post_image_id)
+                else:
+                    post_image = 'https://craigslist.org/images/peace.jpg'
+
+                final_postings.append((post_title, post_url, post_price,post_image))
         # print(post_title)
         # print(post_price)
         # print(post_url)
